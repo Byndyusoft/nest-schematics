5f5a75c26422ddd1d7d1b5c55b22445dc196108f
Sergey Sokolov <xxxARGUSxxx58@gmail.com>
2023-06-06 11:45:11 +0500

    feat: esLint ignore files

--- src/app/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { AppOptions } from "./index.schema";
-export declare function main(options: AppOptions): Rule;
-//# sourceMappingURL=index.d.ts.map
